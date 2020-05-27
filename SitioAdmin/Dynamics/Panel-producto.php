333fbe57600e394790c0c55ad6b481d04d18b66e
emilianole6312 <emilianole0312@comunidad.unam.mx>
2020-05-27 13:58:33 -0500

    FEAT: Se añadio el panel de opciones relacionadas con los productos.

---
+++ SitioAdmin/Dynamics/Panel-producto.php
@@ -0,0 +1,28 @@
+<?php
+  session_id();
+  session_start();
+  //if (isset($_SESSION['IDK']))
+  //{
+    echo "<!DOCTYPE html>
+            <html lang='es' dir='ltr'>
+              <head>
+                <meta charset='utf-8'>
+                <title> Panel de control (Productos) </title>
+              </head>
+              <body>
+                <div class='contenedor'>
+                  <fieldset>
+                    <legend> <h1> Que vamos a hacer hoy? </h1> </legend>
+                      <h3><a href='#'> Añadir producto </a> </h3>
+                      <br>
+                      <h3><a href='#'> Modificar un producto existente </a> </h3>
+                      <br>
+                      <h3><a href='#'> Eliminar producto </a> </h3>
+                  </fieldset>
+                </div>
+              </body>
+            </html>";
+  //}
+  //else
+    //header('location: ../Templates/Inicio_sesion.html')
+?>
